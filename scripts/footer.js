151c3aef77f72b85c86661e7d44bb6aae80cbfd3
Tigr <147647935+TigranAko@users.noreply.github.com>
2023-12-03 11:13:06 +0300

    Create footer.js

---
+++ scripts/footer.js
@@ -0,0 +1,20 @@
+footer = document.getElementsByClassName('footer')[0]
+console.log(footer)
+footer.innerHTML = `        <div class="rights">
+            <p>
+                All Rights Reserved © 2023
+            </p>
+            <p>
+                Все права защищены © 2023
+            </p>
+        </div>
+        <div>
+            <p>
+                <a href="https://t.me/gangster_05">Котакты</a>
+            </p>
+            <p>
+                <a href="https://t.me/TigranAko">Работа</a>
+            </p>
+            <p>
+                <a href="https://t.me/TigranAko">Наша команда</a>
+            </p>`
